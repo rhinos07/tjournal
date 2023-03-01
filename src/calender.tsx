8ac85c6ad3a05d9a851601ca963fa11bc27917d4
rhinos07 <rhinos07@users.noreply.github.com>
2023-03-01 16:09:31 +0100

    editing

--- src/calender.tsx
+++ src/calender.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
+import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
   MonthView,
   Toolbar,
   DateNavigator,
   Appointments,
+  AppointmentTooltip,
+  AppointmentForm,
   TodayButton,
+  ConfirmationDialog,
 } from '@devexpress/dx-react-scheduler-material-ui';
 import {
   BaseDirectory,
@@ -15,8 +18,8 @@ import {
 } from "@tauri-apps/api/fs";
 
 const schedulerData = [
-  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+  { startDate: '2023-03-01T09:45', endDate: '2023-03-01T11:00', title: 'Meeting' },
+  { startDate: '2023-03-02T12:00', endDate: '2023-03-02T13:30', title: 'Go to a gym' },
 ];
 
 interface IProps {
@@ -43,10 +46,28 @@ export default class Calender extends React.PureComponent<IProps, IState> {
     }; 
 
     this.currentDateChange = (currentDate) => { this.setState({ currentDate }); };
-
+    this.commitChanges = this.commitChanges.bind(this);
 
     //readFile1();
 
+  }
+
+  commitChanges({ added, changed, deleted }) {
+    this.setState((state) => {
+      let { data } = state;
+      if (added) {
+        const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+        data = [...data, { id: startingAddedId, ...added }];
+      }
+      if (changed) {
+        data = data.map(appointment => (
+          changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+      }
+      if (deleted !== undefined) {
+        data = data.filter(appointment => appointment.id !== deleted);
+      }
+      return { data };
+    });
   }
 
 
@@ -75,11 +96,24 @@ export default class Calender extends React.PureComponent<IProps, IState> {
             currentDate={currentDate}
             onCurrentDateChange={this.currentDateChange}
           />
+                    <EditingState
+            onCommitChanges={this.commitChanges}
+          />
+          <IntegratedEditing />
           <MonthView />
           <Toolbar />
           <DateNavigator />
           <TodayButton />
           <Appointments />
+          <ConfirmationDialog
+            ignoreCancel
+          />
+          <AppointmentTooltip
+            showCloseButton
+            showOpenButton
+          />
+          <AppointmentForm
+          />
         </Scheduler>
       </Paper>
     );
